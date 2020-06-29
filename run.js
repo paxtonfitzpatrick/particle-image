c1c21b98d5f7b402c44bdae8443c308ac58bd009
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-06-29 12:29:04 -0400

    call from separate script

---
+++ run.js
@@ -0,0 +1,49 @@
+"use strict";
+
+particleImageDisplay('particle-animation',
+  {
+    "particles": {
+      "density": 200,
+      "color": "#fff",
+      "opactiy": {
+        "value": 1
+      },
+      "size": {
+        "value": 1,
+        "random": false,
+      },
+      "movement": {
+        "speed": 1,
+      },
+      "interactivity": {
+        "on_hover": {
+          "enabled": true,
+          "action": "repulse"
+        },
+        "on_click": {
+          "enabled": false,
+          "action": "big_repulse"
+        },
+        "on_touch": {
+          "enabled": true,
+          "action": "repulse"
+        },
+      }
+    },
+    "image": {
+      "size": {
+        "width_pct": 60,
+        "min_px": 150,
+        "max_px": 800
+      }
+    },
+    "interactions": {
+      "repulse": {
+        "distance": 100,
+        "strength": 200
+      }
+    },
+    "retina_detect": false,
+    "disabled": false
+  }
+);
